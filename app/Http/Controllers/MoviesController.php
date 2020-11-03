a6a8d74342a0336ec48bae66013ccb99b2b554ec
fakhri ahnaf <fakhri_ahnaf01@yahoo.co.id>
2020-11-03 13:13:31 +0700

    movie controller adding

---
+++ app/Http/Controllers/MoviesController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MoviesController extends Controller
+{
+    public function index() 
+    {
+        $popularMovies = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/popular')
+            ->json()['results'];
+
+        $nowPlayingMovies = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/now_playing')
+            ->json()['results'];
+
+        $genres = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/genre/movie/list')
+            ->json()['genres'];
+
+         $viewModel = new MoviesViewModel(
+            $popularMovies,
+            $nowPlayingMovies,
+            $genres,
+        );
+
+        return view('movies.index', $viewModel);
+        
+    }
+
+    public function show($id)
+    {
+        $movie = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/'.$id.'?append_to_response=credits,videos,images')
+            ->json();
+
+        $viewModel = new MovieViewModel($movie);
+
+        return view('movies.show', $viewModel);
+    }
+
+
+}
